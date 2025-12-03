f95e6310986c96421cb76c5cba87f52540e8129b
YamsTheBe <Mariamadiaw.a@hotmail.fr>
2025-12-03 23:14:08 +0100

    created homePage

---
+++ frontend/src/page/home.tsx
@@ -0,0 +1,30 @@
+// --- 9. COMPOSANT HOME (Assemblage des composants) ---
+
+import HeroSlider from "../components/HeroSlider";
+import { MissionSection } from "../components/MissionSection";
+import "../components/GalerieImpact";
+import { FaitesUnDon } from "../components/FaitesUnDon";
+import GalerieImpact from "../components/GalerieImpact";
+const Home: React.FC = () => {
+	return (
+		<div className="home-page">
+			<section className="hero-section">
+				<HeroSlider />
+			</section>
+
+			<section className="mission-section">
+				<MissionSection />
+			</section>
+
+			<section className="galerieImpact-section">
+				<GalerieImpact />
+			</section>
+
+			<section className="FaitesUnDon-section">
+				<FaitesUnDon />
+			</section>
+		</div>
+	);
+};
+
+export default Home;
